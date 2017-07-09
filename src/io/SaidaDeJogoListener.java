0e9c29ae6ae0a68d3af6feced2b4e4a2443afa6e
Raydson Ferreira carlota <raydsonferreira@gmail.com>
2017-07-09 12:01:57 -0300

    Adicionadado Linha

--- src/io/SaidaDeJogoListener.java
+++ src/io/SaidaDeJogoListener.java
@@ -3,6 +3,9 @@ package io;
 /**
  * Interface Printavel - Define métodos para saída de dados.
  * 
+ *  Esta classe eh parte da aplicacao "World of Zuul - O Manicômio de Zulu".
+ * "World of Zuul" é um jogo de aventura muito simples, baseado em texto.  
+ * 
  * Esta interface serve para assegurar que a classe que a implementar irá
  * suportar a capacidade de imprimir os dados fornecidos a ela em um campo
  * qualquer, seja no console, seja em uma caixa de texto em uma GUI, etc.
